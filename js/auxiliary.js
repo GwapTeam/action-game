76ae1cb08a82d57eae29d68a83f84058c5ffe2dc
dorudorujr <a1209s518n928@gmail.com>
2017-10-23 14:15:06 +0900

    add auxiliary.js  update complete-action-game.js,html

---
+++ js/auxiliary.js
@@ -0,0 +1,17 @@
+function mapSet(blocks,blockset){
+    var count = 0;
+    for(var i = 0;i<20;i++){
+        for(var j = 0;j<blockset.length;j++){
+            count = blockset[j];
+            if(i>=15-count && i<15){
+                if(i == (15-count)){
+                    blocks[i][j] = 4;
+                }else{
+                    blocks[i][j] = 3;
+                }
+            }else{
+                blocks[i][j] = -1;
+            }
+        }
+    }
+}
